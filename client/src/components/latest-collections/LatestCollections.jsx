340334f2cafd57751885a074ba17653bc2792242
Joseph Pal <josephpal27@gmail.com>
2026-02-08 23:58:46 +0530

    Latest Collections Updated

--- client/src/components/latest-collections/LatestCollections.jsx
+++ client/src/components/latest-collections/LatestCollections.jsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
 import './LatestCollections.css';
 
 const LatestCollections = () => {
     return (
         <>
-
+            <section className="latest-collections">
+                <img src="/images/latest-collections-bg.png" alt="Latest Collections" loading="lazy" />
+                <div className="latest-collection-content">
+                    <h3><span>LATEST</span> COLLECTIONS</h3>
+                    <Link to="/collections">View Details</Link>
+                </div>
+            </section>
         </>
     )
 }
