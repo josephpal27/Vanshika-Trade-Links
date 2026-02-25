980233f7b8989fc0c0418d9838c9ca1740230acf
Joseph Pal <josephpal27@gmail.com>
2026-02-25 18:07:28 +0530

    Collections Updated

--- client/src/components/our-products/OurProducts.jsx
+++ client/src/components/our-products/OurProducts.jsx
@@ -14,10 +14,14 @@ const OurProducts = () => {
 
                 {/* Products */}
                 <div className="products-container">
-                    {/* Collections */}
-                    <div className="collections">
-                        <span id="collections-head">Our Collections</span>
-                        <div className="collections-box">
+                    {/* Head */}
+                    <div className="product-container-head">
+                        <h2>Our Collections</h2>
+                    </div>
+                    {/* Body */}
+                    <div className="product-container-body">
+                        {/* Collections */}
+                        <div className="collections">
                             <Link to="/category/rainbow-series" className="active">Rainbow Series</Link>
                             <Link to="/category/vital-series">Vital Series</Link>
                             <Link to="/category/ecoline-series">Ecoline Series</Link>
@@ -25,18 +29,18 @@ const OurProducts = () => {
                             <Link to="/category/lily-series">Lily Series</Link>
                             <Link to="/category/cosmo-jr-series">Cosmo Jr Series</Link>
                         </div>
-                    </div>
-                    {/* Products Row */}
-                    <div className="products-row">
-                        {/* Product Cards Row */}
-                        <div className="products-cards-row">
-                            <div className="product-card">
-                                <div className="card-image">
-                                    <img src="/images/products/RNB-1201.png" alt="Product 1" loading="lazy" />
-                                </div>
-                                <div className="card-desc">
-                                    <span>Pillar Cock</span>
-                                    <Link to="/product-details">Explore</Link>
+                        {/* Products Row */}
+                        <div className="products-row">
+                            {/* Product Cards Row */}
+                            <div className="products-cards-row">
+                                <div className="product-card">
+                                    <div className="card-image">
+                                        <img src="/images/products/RNB-1201.png" alt="Product 1" loading="lazy" />
+                                    </div>
+                                    <div className="card-desc">
+                                        <span>Pillar Cock</span>
+                                        <Link to="/product-details">Explore</Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
