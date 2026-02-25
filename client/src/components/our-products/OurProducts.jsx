8f52f38c121ea08335c04c76edcc37c61fe88854
Joseph Pal <josephpal27@gmail.com>
2026-02-25 17:47:41 +0530

    Products Updated

--- client/src/components/our-products/OurProducts.jsx
+++ client/src/components/our-products/OurProducts.jsx
@@ -14,44 +14,22 @@ const OurProducts = () => {
 
                 {/* Products */}
                 <div className="products-container">
-                    {/* Filters */}
-                    <div className="filters">
-                        <span>Our Collections</span>
-                        <div className="filter-box">
-                            <span>By Category</span>
+                    {/* Collections */}
+                    <div className="collections">
+                        <span id="collections-head">Our Collections</span>
+                        <div className="collections-box">
+                            <Link to="/category/rainbow-series" className="active">Rainbow Series</Link>
+                            <Link to="/category/vital-series">Vital Series</Link>
+                            <Link to="/category/ecoline-series">Ecoline Series</Link>
+                            <Link to="/category/studio-series">Studio Series</Link>
+                            <Link to="/category/lily-series">Lily Series</Link>
+                            <Link to="/category/cosmo-jr-series">Cosmo Jr Series</Link>
                         </div>
                     </div>
                     {/* Products Row */}
                     <div className="products-row">
-                        {/* Product Cards */}
+                        {/* Product Cards Row */}
                         <div className="products-cards-row">
-                            <div className="product-card">
-                                <div className="card-image">
-                                    <img src="/images/products/RNB-1201.png" alt="Product 1" loading="lazy" />
-                                </div>
-                                <div className="card-desc">
-                                    <span>Pillar Cock</span>
-                                    <Link to="/product-details">Explore</Link>
-                                </div>
-                            </div>
-                            <div className="product-card">
-                                <div className="card-image">
-                                    <img src="/images/products/RNB-1201.png" alt="Product 1" loading="lazy" />
-                                </div>
-                                <div className="card-desc">
-                                    <span>Pillar Cock</span>
-                                    <Link to="/product-details">Explore</Link>
-                                </div>
-                            </div>
-                            <div className="product-card">
-                                <div className="card-image">
-                                    <img src="/images/products/RNB-1201.png" alt="Product 1" loading="lazy" />
-                                </div>
-                                <div className="card-desc">
-                                    <span>Pillar Cock</span>
-                                    <Link to="/product-details">Explore</Link>
-                                </div>
-                            </div>
                             <div className="product-card">
                                 <div className="card-image">
                                     <img src="/images/products/RNB-1201.png" alt="Product 1" loading="lazy" />
