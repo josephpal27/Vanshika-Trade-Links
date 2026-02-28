876b562be804ecec72c73892c30198664862af84
Joseph Pal <josephpal27@gmail.com>
2026-02-28 15:44:41 +0530

    Footer bg updated

--- client/src/components/category-list/CategoryList.jsx
+++ client/src/components/category-list/CategoryList.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { categoryData } from '../../data/categoryData';
 import CategoryCard from './CategoryCard';
 import './CategoryList.css';
@@ -11,14 +10,6 @@ const CategoryList = () => {
                     {
                         categoryData.map((category, index) => {
                             return (
-                                // <Link to={`/category/${category.slug}`} className="category-card" key={index}>
-                                //     <div className="card-image">
-                                //         <img src={category.image} alt="Category Image" loading="lazy" />
-                                //     </div>
-                                //     <div className="card-desc">
-                                //         <span>{category.name}</span>
-                                //     </div>
-                                // </Link>
                                 <CategoryCard category={category} key={index} />
                             )
                         })
