b0c79a120ab165d441fce355e0e6e5324035a3db
Joseph Pal <josephpal27@gmail.com>
2026-03-12 12:50:46 +0530

    Product Banner and Gallery made dynamic

--- client/src/components/product-details-banner/ProductDetailsBanner.jsx
+++ client/src/components/product-details-banner/ProductDetailsBanner.jsx
@@ -1,20 +1,30 @@
 import { Link, useParams } from 'react-router-dom';
 import { categoryData } from '../../data/categoryData';
+import { productsData } from '../../data/productsData';
 import './ProductDetailsBanner.css';
 
 const ProductDetailsBanner = () => {
 
-    const { categorySlug } = useParams();
+    const { categorySlug, productSlug } = useParams();
+
+    const product = productsData[categorySlug]?.find(
+        (p) => p.slug === productSlug
+    );
 
     return (
         <>
             <section className="product-details-banner">
                 <img src="/images/banners/product-details-banner.png" alt="Products Banner" loading="lazy" />
                 <div className="banner-content">
-                    {/* <h1>{categoryData.find(cat => cat.slug === categorySlug)?.name || "Products"}</h1> */}
-                    <h1>Pillar Cock Regular</h1>
-                    {/* <span><Link to="/">Home</Link> / <Link to="/category">Category</Link> / {categoryData.find(cat => cat.slug === categorySlug)?.name || "Products"}</span> */}
-                    <span><Link to="/">Home</Link> / <Link to="/category">Category</Link> / <Link to="">Rainbow Series</Link> / Pillar Cock Regular</span>
+                    <h1>{product?.name || "Product Name"}</h1>
+                    <span>
+                        <Link to="/">Home</Link> / {' '}
+                        <Link to="/category">Category</Link> / {' '}
+                        <Link to={`/category/${categorySlug}`}>
+                            {categoryData.find(cat => cat.slug === categorySlug)?.name || "Products Category"} {' '}
+                        </Link> {' '}
+                        / {product?.name || "Product Name"}
+                    </span>
                 </div>
             </section>
         </>
