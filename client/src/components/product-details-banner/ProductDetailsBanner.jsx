7b074fcaede89cae0bea36186b1056c4486aa03a
Joseph Pal <josephpal27@gmail.com>
2026-03-12 10:49:30 +0530

    Product Banner Updated

--- client/src/components/product-details-banner/ProductDetailsBanner.jsx
+++ client/src/components/product-details-banner/ProductDetailsBanner.jsx
@@ -8,13 +8,13 @@ const ProductDetailsBanner = () => {
 
     return (
         <>
-            <section className="products-banner">
+            <section className="product-details-banner">
                 <img src="/images/banners/products-banner.png" alt="Products Banner" loading="lazy" />
                 <div className="banner-content">
                     {/* <h1>{categoryData.find(cat => cat.slug === categorySlug)?.name || "Products"}</h1> */}
                     <h1>Product Name</h1>
                     {/* <span><Link to="/">Home</Link> / <Link to="/category">Category</Link> / {categoryData.find(cat => cat.slug === categorySlug)?.name || "Products"}</span> */}
-                    <span>Home / Category / Series / Product Name</span>
+                    <span><Link to="/">Home</Link> / <Link to="/category">Category</Link> / <Link to="">Series</Link> / Product Name</span>
                 </div>
             </section>
         </>
