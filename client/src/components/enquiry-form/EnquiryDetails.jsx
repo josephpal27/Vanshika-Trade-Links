288a7f6ad55cb21c7bbdd5f1245eb2046cf35f54
Joseph Pal <josephpal27@gmail.com>
2026-03-12 15:41:40 +0530

    Enquiry Page Added

--- client/src/components/enquiry-form/EnquiryDetails.jsx
+++ client/src/components/enquiry-form/EnquiryDetails.jsx
@@ -16,6 +16,19 @@ const EnquiryDetails = () => {
           </div>
         </div>
 
+        <div className="enquiry-form">
+          <form action="">
+            <input type="text" name="Product" placeholder="Product Name" required value={'Pillar Cock Regular'} />
+            <input type="text" name="Category" placeholder="Product Category" required value={'Rainbow Series'} />
+            <input type="text" name="Name" placeholder="Full Name" required />
+            <input type="text" name="Email" placeholder="Email Id" required />
+            <input type="text" name="Phone" placeholder="Phone" required />
+            <input type="text" name="Subject" placeholder="Subject" />
+            <textarea name="Message" placeholder="Message"></textarea>
+            <button type="submit">SEND MESSAGE</button>
+          </form>
+        </div>
+
       </section>
     </>
   );
